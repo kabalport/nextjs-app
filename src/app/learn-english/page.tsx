4d1717f568c269669f3380a4afe148cdbe92eed8
최대열 <dychoi@brainednet.com>
2023-10-26 13:56:40 +0900

    test55

--- src/app/learn-english/page.tsx
+++ src/app/learn-english/page.tsx
@@ -1,42 +1,9 @@
-'use client'
 import Header from "@/components/learn-english/header";
 import styles from './style.module.css';
-import {useState} from "react";
 
 
 
 export default async function Detail() {
-    const [after, setAfter] = useState('');
-    const handleSubmitSentence = async (sentence) => {
-
-
-        try {
-            const response = await fetch('/api/generate', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ before: sentence }),
-            });
-
-            const data = await response.json();
-            if (response.status !== 200) {
-                throw (
-                    data.error ||
-                    new Error(`request failed with status ${response.status}`)
-                );
-            }
-
-            if (data.after.trim() == 'Not Found') {
-                setAfter('올바른 표현을 찾을 수 없습니다.');
-            } else {
-                setAfter(data.after.trim());
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
     return (
         <>
            <Header />
