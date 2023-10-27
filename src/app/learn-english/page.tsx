c8e4087b183ba336275d50c5b56c94a01ec9916e
최대열 <dychoi@brainednet.com>
2023-10-27 13:47:01 +0900

    chatgpt init

--- src/app/learn-english/page.tsx
+++ src/app/learn-english/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/learn-english/header";
 import styles from './style.module.css';
 
-import ChatGptItem from "@/components/learn-english/ChatGptItem";
+
 
 
 
@@ -10,7 +10,6 @@ export default async function Detail() {
 
     return (
         <>
-            <ChatGptItem/>
            <Header />
             <div className={styles.wrapper}>
                 <h2>test</h2>
