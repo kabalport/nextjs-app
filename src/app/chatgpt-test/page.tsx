bb3b1fe49c26c8a81b72a961aa71136c37799103
kabalport <kabalport@gmail.com>
2023-10-28 17:56:08 +0900

    gpt test3

--- src/app/chatgpt-test/page.tsx
+++ src/app/chatgpt-test/page.tsx
@@ -1,18 +1,12 @@
 import Header from "@/components/chatgpt-test/header";
 import styles from './style.module.css';
 import ChatGptItem from "@/components/chatgpt-test/ChatGptItem";
-import {getGpt} from "@/utils/chatgpt-test/getGpt";
-
 
 
 export default async function ChatGptTestPage() {
-
-    const {choices} = await getGpt();
-
     return (
         <>
            <Header />
-            <h1>{choices[0].message.content}</h1>
             <div className={styles.wrapper}>
                 <ChatGptItem />
             </div>
--- src/components/chatgpt-test/ChatGptItem.tsx
+++ src/components/chatgpt-test/ChatGptItem.tsx
@@ -1,10 +1,15 @@
+import {getGpt} from "@/utils/chatgpt-test/getGpt";
 
 
 export default async function ChatGptItem() {
-
+    const {choices} = await getGpt();
     return (
-        <>
+        <div>
             <h1>test</h1>
-        </>
+                <h1>{choices.map((res) => (
+                    <h1>{res.message.content}</h1>
+                ))}
+                </h1>
+        </div>
     )
 }
