b962d64b4ff402e0b15a170d51bc8d8ccc8c6e2f
최대열 <dychoi@brainednet.com>
2023-10-31 09:34:25 +0900

    diary dummyData55222

--- src/components/gpt-diary/getDiaryInput.tsx
+++ src/components/gpt-diary/getDiaryInput.tsx
@@ -5,28 +5,36 @@ import {getDiary} from "@/utils/gpt-diary/getDiary";
 
 export default function GetDiaryInput() {
     const [userInput, setUserInput] = useState("");
+    const [message, setMessage] = useState([]); // Add a state for the message
+
     // 사용자의 입력을 받아, 상위컴포넌트로 데이터를 전달
+
 
     // loading 상태 - 사용자가 제출버튼을 못 누르도록 처리
     const handleUserInput = (e: any) => {
         setUserInput(e.target.value);
     };
-    const handleClick = () => {
+    const handleClick = async () => {
         if (!userInput) {
             console.log('일과를 적어주세요');
             return;
         }
         console.log("GPT에 전달될 회고록 내용:", userInput);
-        try{
-            const message = getDiary(`${userInput}`);
-            console.log(message)    
-        }catch (error: any){
-            console.log('에러')
+        try {
+            const response = await getDiary(`${userInput}`);
+            if (response.choices && response.choices.length > 0) {
+                // 여기에서 choices 배열 내부의 message.content 값을 추출하여 상태에 저장합니다.
+                const diaryMessages = response.choices.map(choice => choice.message.content);
+                setMessage(diaryMessages);
+            }
+        } catch (error: any) {
+            console.log('에러:', error.message);
             return;
-        }finally {
-            console.log('final')
+        } finally {
+            console.log('final');
         }
     };
+
 
     return (
         <div>
@@ -42,7 +50,7 @@ export default function GetDiaryInput() {
                     GPT 회고록을 작성해줘!
                 </button>
             </div>
-            <canvas id="canvas" className={styles.canvas}></canvas>
+            <h1>{message}</h1>
         </div>
     )
 }
